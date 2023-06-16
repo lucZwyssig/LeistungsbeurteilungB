51b3e3ed4fe167cf216615a5ac035b270945475d
luczwyssig <77100623+thefaceslayer12@users.noreply.github.com>
2023-06-16 15:15:04 +0200

    feature: eslint installiert, Fix: verschiedene statuscode s richtig gemacht

--- Code/Endpoints.js
+++ Code/Endpoints.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 const session = require('express-session')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(
     session({
@@ -13,7 +16,7 @@ app.use(
 )
 // app.use von alten code kopiert
 
-const tasks = [
+let tasks = [
     { "id": "2020323", "createdDate": "2023-06-15", "completedDate": null, "title": "23423", "email": "luc@gmail.com" },
     { "id": "334344344", "createdDate": "2023-06-14", "completedDate": "2023-06-16", "title": "3434344", "email": "cul@liamg.com" },
     { "id": "23020302323", "createdDate": "2023-06-12", "completedDate": null, "title": "Task 3", "email": "luc@gmail.com" },
@@ -33,6 +36,33 @@ const checkEmail = (email) => {
 }
 
 app.post('/login', (req, res) => {
+    /* #swagger.parameters['credentials'] = {
+    in: 'body',
+    description: 'The credentials of the user. They must be correct to receive access to the API',
+    required: true,
+    schema: {
+      email: 'email with an @ for example 123@321.com',
+      password: 'string'
+    }
+  } */
+
+    /* #swagger.responses[200] = {
+              description: 'User successfully logged in .',
+              
+      } */
+
+    /* #swagger.responses[415] = {
+            description: 'User entered wrong email schema',
+            schema: 'not an email' 
+    } */
+    /* #swagger.responses[401] = {
+            description: 'Not autherized',
+            
+    } */
+    /* #swagger.responses[40] = {
+            description: 'user did not enter email',
+            6
+    } */
 
     if (!req.is('json')) {
         res.sendStatus(415)
@@ -41,7 +71,7 @@ app.post('/login', (req, res) => {
     const emailInput = req.body.email
     const isEmail = checkEmail(emailInput)
     if (!isEmail) {
-        res.status(415).json("not an email")
+        res.status(406).json("not an email")
         return
     }
 
@@ -59,7 +89,7 @@ app.post('/login', (req, res) => {
 
 const verify = (req, res, next) => {
     if (!req.session.email) {
-        res.sendStatus(401)
+        res.sendStatus(403)
     }
     else {
         next()
@@ -68,6 +98,20 @@ const verify = (req, res, next) => {
 
 
 app.get('/verify', (req, res) => {
+
+
+    /* #swagger.responses[200] = {
+              description: 'User successfully logged in .',
+              schema: {status: "you are logged in", cookie: "123@321.com"}
+              
+      } */
+
+    /* #swagger.responses[401] = {
+            description: 'Not autherized',
+            schema: {status: "you are not logged in" }
+            
+            
+    } */
     if (!req.session.email) {
         res.status(401).json({
             "status": "you are not logged in"
@@ -83,6 +127,13 @@ app.get('/verify', (req, res) => {
 })
 
 app.delete('/logout', (req, res) => {
+
+    /* #swagger.responses[204] = {
+              description: 'User successfully logged out .',
+              
+      } */
+
+
     req.session.destroy();
     res.sendStatus(204)
 })
@@ -90,18 +141,55 @@ app.delete('/logout', (req, res) => {
 
 
 app.get('/tasks', verify, (req, res) => {
+
+    /* #swagger.responses[200] = {
+              description: 'User recieves objects .',
+              schema: [{ "id": "2020323", "createdDate": "2023-06-15", "completedDate": null, "title": "23423", "email": "luc@gmail.com" },
+      { "id": "334344344", "createdDate": "2023-06-14", "completedDate": "2023-06-16", "title": "3434344", "email": "cul@liamg.com" }]
+              
+      } */
+    /* #swagger.responses[403] = {
+            description: 'Forbidden',
+            
+    } */
     const userTasks = tasks.filter((task) => task.email === req.session.email)
     res.status(200).json(userTasks)
 })
 
 app.post('/tasks', verify, (req, res) => {
+    /* #swagger.parameters['task'] = {
+    in: 'body',
+    description: 'The task the user wants to post',
+    required: true,
+    schema: {  "createdDate": "2023-06-14", "completedDate": "2023-06-16", "title": "3434344"}
+  } */
+
+    /* #swagger.responses[201] = {
+              description: 'User successfully posted task.',
+              schema: [{ "id": "2020323", "createdDate": "2023-06-15", "completedDate": null, "title": "23423", "email": "luc@gmail.com" },
+      { "id": "334344344", "createdDate": "2023-06-14", "completedDate": "2023-06-16", "title": "3434344", "email": "cul@liamg.com" }]
+              
+      } */
+
+    /* #swagger.responses[415] = {
+            description: 'unsupported media type'
+            
+    } */
+    /* #swagger.responses[406] = {
+            description: 'task doesnt follow schema'
+            
+    } */
+    /* #swagger.responses[403] = {
+            description: 'Forbidden',
+            
+    } */
     const userTasks = tasks.filter((task) => task.email === req.session.email)
     const newTask = req.body
     if (!req.is("json")) {
         res.sendStatus(415)
     }
     else if (!newTask.title || !newTask.createdDate) {
-        res.send("no")
+        res.sendStatus(406)
     }
     else {
         const id = Math.floor(Math.random() * 10000000 + 1).toString();
@@ -129,10 +217,33 @@ app.post('/tasks', verify, (req, res) => {
 })
 
 app.get('/tasks/:id', verify, (req, res) => {
+    /* #swagger.parameters['id'] = {
+   in: 'path',
+   description: 'ID of the task that the user wants to get',
+   required: true,
+   type: 'string'
+} */
+
+    /* #swagger.responses[200] = {
+              description: 'gets task.',
+              schema:  { "id": "334344344", "createdDate": "2023-06-14", "completedDate": "2023-06-16", "title": "3434344", "email": "cul@liamg.com" }
+              
+      } */
+
+    /* #swagger.responses[403] = {
+            description: 'Forbidden',
+            
+    } */
+    /* #swagger.responses[404] = {
+            description: 'task not found',
+            
+    } */
+
+
     const userTasks = tasks.filter((task) => task.email === req.session.email)
     const taskID = req.params.id;
-    const singleTask = userTasks.find((task) => task.id === taskID)
-    if (task === -1) {
+    const singleTask = userTasks.findIndex((task) => task.id === taskID)
+    if (singleTask === -1) {
         res.sendStatus(404)
     } else {
         res.status(200).send(userTasks[singleTask])
@@ -140,6 +251,35 @@ app.get('/tasks/:id', verify, (req, res) => {
 })
 
 app.put('/tasks/:id', verify, (req, res) => {
+    /* #swagger.parameters['id'] = {
+    in: 'path',
+    description: 'ID of the task that the user wants to replace',
+    required: true,
+    type: 'string'
+} */
+
+    /* #swagger.responses[201] = {
+              description: 'updates task.',
+              schema:  { "id": "334344344", "createdDate": "2023-06-14", "completedDate": "2023-06-16", "title": "3434344", "email": "cul@liamg.com" }
+              
+      } */
+
+    /* #swagger.responses[403] = {
+            description: 'Forbidden',
+            
+    } */
+    /* #swagger.responses[404] = {
+            description: 'task not found',
+            
+    } */
+    /* #swagger.responses[415] = {
+            description: 'task in not supported media type',
+            
+    } */
+    /* #swagger.responses[406] = {
+            description: 'task not in correct format',
+            
+    } */
     const userTasks = tasks.filter((task) => task.email === req.session.email)
     const taskID = req.params.id
     const newTask = req.body
@@ -147,8 +287,9 @@ app.put('/tasks/:id', verify, (req, res) => {
 
     if (!req.is("json")) {
         res.sendStatus(415)
+
     } else if (!newTask.title || !newTask.createdDate) {
-        res.send("no")
+        res.status(406).send("your task does not match criteria")
     }
 
     else if (taskIndex === -1) {
@@ -178,6 +319,28 @@ app.put('/tasks/:id', verify, (req, res) => {
 })
 
 app.delete('/tasks/:id', verify, (req, res) => {
+    /* #swagger.parameters['id'] = {
+    in: 'path',
+    description: 'ID of the task that the user wants to delete',
+    required: true,
+    type: 'string'
+} */
+
+    /* #swagger.responses[204] = {
+              description: 'deleted task.'
+              
+      } */
+
+    /* #swagger.responses[403] = {
+            description: 'Forbidden',
+            
+    } */
+    /* #swagger.responses[404] = {
+            description: 'task not found',
+            
+    } */
+   
+    
     const userTasks = tasks.filter((task) => task.email === req.session.email)
     const taskID = req.params.id
     const taskIndex = userTasks.findIndex((task) => task.id === taskID)
@@ -186,28 +349,20 @@ app.delete('/tasks/:id', verify, (req, res) => {
     }
     else {
         tasks.splice(tasks.findIndex((task) => task.id === taskID), 1)
-        res.sendStatus(200)
+        res.sendStatus(204)
     }
 
 })
 
+
+
 //Das mit dem stern von https://stackoverflow.com/questions/52552150/how-to-deal-when-calling-a-wrong-endpoint-using-app-get
-app.get('*', (req, res) => {
+
+
+app.all('/*', (req, res) => {
     res.sendStatus(404)
 })
 
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, () => console.log("server started"));
 
-//do Authentifizierung #9
-// do change no if wrong format
+
---
+++ Code/swagger.js
@@ -0,0 +1,8 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const outputFile = './swagger_output.json'
+const endpointsFile = ['Endpoints.js'];
+
+swaggerAutogen(outputFile, endpointsFile);
+
+//code von library api
